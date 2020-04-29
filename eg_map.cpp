882d0ae256bd8fed416f593354089beaeee17b32
Rashmi <rashmi.kare@gmail.com>
2020-04-29 10:36:44 +0100

    map example

--- eg_map.cpp
+++ eg_map.cpp
@@ -7,14 +7,55 @@
 #include <map>
 #include <unordered_map>
 
-uisng namespace std;
+using namespace std;
 
 int main()
 {
 	map<int, string> this_map;
 	int temp;
 	string str;
+	pair<int,string> p;
 
 	
+	cout << "Inserting..."<<endl;
+	while(temp >= 0)
+	{
+		cout<<"Enter key: ";
+		getline(cin,str);
+		temp = stoi(str);
+		if(temp>=0)
+		{
+			p.first=temp;
+			cout<<"Enter name ";
+			getline(cin,str);
+			p.second=str;
+			this_map.insert(p);
+		}
+	}
+
+	cout<<"{  ";
+	for(auto it = this_map.begin(); it != this_map.end(); it++)
+		cout << it->first << "->" << it->second << ",  ";
+	cout << "}\n";
+	temp = 0;
+	
+	cout << "Removing..."<<endl;
+	while(temp >= 0)
+	{
+		cout<<"Enter key: ";
+		getline(cin,str);
+		temp = stoi(str);
+		if(temp>=0)
+		{
+			this_map.erase(temp);
+		}
+	}
+
+	cout<<"{  ";
+	for(auto it = this_map.begin(); it != this_map.end(); it++)
+		cout << it->first << "->" << it->second << ",  ";
+	cout << "}";
+
+
 	return 0;
 }
